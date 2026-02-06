a319b9c64a536e0c08d2efa4199f86819ae7ca3f
HigorMega <99227325+HigorMega@users.noreply.github.com>
2026-02-06 18:59:32 -0300

    Add JSON webhook logging endpoint

--- public_html/api/ping_json.php
+++ public_html/api/ping_json.php
@@ -1,5 +1,12 @@
 <?php
+declare(strict_types=1);
+
+ini_set('display_errors', '0');
+error_reporting(E_ALL);
+
 header('Content-Type: application/json; charset=utf-8');
+http_response_code(200);
+
 echo json_encode([
     'ok' => true,
     'time' => date('c'),
